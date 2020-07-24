7cc0d3c15c496aff353a57965339c2fbe126651f
Ian Nájar <ianenri@gmail.com>
2020-07-24 00:31:46 -0600

    .\src\components\tablebasic\tablebasic.component.jsx

--- src/components/tablebasic/tablebasic.component.jsx
+++ src/components/tablebasic/tablebasic.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 //import defaultdata from './defaultdata.json'
 
 const TableBasicComponent = ({title, children, ...otherprops }) => {  
+    if (children.type === "table"){
     return(<div {...otherprops}>
          <div className="card">
             <div className="card-title">
@@ -13,7 +14,12 @@ const TableBasicComponent = ({title, children, ...otherprops }) => {
                 </div>
             </div>
          </div>
-    </div>)
+    </div>)}
+    else{
+        console.log("You should provide a table inside this component")
+        return(<div></div>)
+    }
+    
 }
 
 export default TableBasicComponent
